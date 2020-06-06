e333861855e3f24833c6a8840bd19dfdd72b0a3d
Agustin Villar <amauro85@gmail.com>
2020-06-06 18:54:35 +0200

    improving sorting, fixing test scenarios

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import './App.css';
 
 import Articles from './components/Articles';
 
+function sortByVotes(data) {
+  const dataToSort = [...data];
+  return dataToSort.sort((itemA, itemB) => itemB['upvotes'] - itemA['upvotes']);
+}
+
+function sortByDate(data) {
+  const dataToSort = [...data];
+  return dataToSort.sort((itemA, itemB) => new Date(itemB['date']) - new Date(itemA['date']));
+}
+
 const App = ({ articles }) => {
-  const [sortedData, setSortedData] = useState(articles);
-
-  function sortByVotes(data) {
-    const dataToSort = [...data];
-    setSortedData(dataToSort.sort((itemA, itemB) => itemA['upvotes'] - itemB['upvotes']));
-  }
-
-  function sortByDate(data) {
-    const dataToSort = [...data];
-    setSortedData(dataToSort.sort((itemA, itemB) => new Date(itemA['date']) - new Date(itemB['date'])));
-  }
+  const [sortedData, setSortedData] = useState(sortByVotes(articles));
 
   return (
     <div className="App">
       <div className="navigation">
-        <button data-testid="top-link" onClick={() => sortByVotes(articles)}>Top</button>
-        <button data-testid="newest-link" onClick={() => sortByDate(articles)}>Newest</button>
+        <button data-testid="top-link" onClick={() => setSortedData(sortByVotes(articles))}>Top</button>
+        <button data-testid="newest-link" onClick={() => setSortedData(sortByDate(articles))}>Newest</button>
       </div>
       <Articles articles={sortedData} />
     </div>
